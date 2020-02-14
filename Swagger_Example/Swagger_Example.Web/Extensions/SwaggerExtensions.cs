d4ddd162691d26dcde8ea444e02d01244d5a35b2
Farhad Zamani <farhad.zamani7720@gmail.com>
2020-02-14 15:19:30 +0330

    Add Solution

---
+++ Swagger_Example/Swagger_Example.Web/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swagger_Example.Web.Extensions
+{
+    public class AuthenticationDocumentFilter : IDocumentFilter
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public AuthenticationDocumentFilter(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
+        {
+            if (!httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+            {
+                swaggerDoc.Definitions = new Dictionary<string, Schema>();
+                swaggerDoc.Paths = new Dictionary<string, PathItem>();
+            }
+        }
+  }
+    public static class SwaggerExtensions
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.EnableAnnotations();
+                options.DocumentFilter<AuthenticationDocumentFilter>();
+                options.SwaggerDoc("v1", new Info { Version = "v1", Title = "Swagger Example" });
+            });
+        }
+        public static void UseSwaggerAndUI(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.DocExpansion(DocExpansion.None);
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Example Docs");
+            });
+        }
+    }
+}
---
+++ Swagger_Example/Swagger_Example.Web/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swagger_Example.Web.Extensions;
+
+namespace Swagger_Example.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            services.AddAuthorization();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.AccessDeniedPath = "/Login";
+                    options.Cookie.HttpOnly = true;
+                    options.LoginPath = "/Login";
+                    options.LogoutPath = "/Login";
+                    options.ExpireTimeSpan = TimeSpan.FromDays(15);
+                    options.SlidingExpiration = true;
+                    options.Cookie.IsEssential = true;
+                    options.ReturnUrlParameter = "returnUrl";
+                });
+            services.AddMvc();
+            services.AddSwagger();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+            app.UseSwaggerAndUI();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                          name: "default",
+                          template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
